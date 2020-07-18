fa12590b7d671bb0522f79e0381872a95e239683
Cara Williams <carawilliams@Castros-MacBook-Pro-2.local>
2020-07-17 20:36:02 -0400

    added loop to calculate change in profit/loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,7 +12,6 @@ with open(budgetdata_csv) as budgetdata_file:
 
     #set initial value of the total_profit_loss variable to 0. this will hold the total sum of profit/loss in the for loop below.
     total_profit_loss = 0
-    total_profit_loss = 0
 
     #define "date" and "profit_loss" variables as lists
     date = []
@@ -22,31 +21,32 @@ with open(budgetdata_csv) as budgetdata_file:
     #iterate through rows of data and add Column A to "data" list and Column B to "profit_loss" list
     for row in csvreader:
        
-        #add each row in date column to "date" list
+        #load each row in date column to "date" list
         date.append(row[0])
         
-        #add each row in "profit_loss" column to "profit_loss" list
+        #load each row in "profit_loss" column to "profit_loss" list
         profit_loss.append(int(row[1]))
- 
-        #in each loop subtract prior "profit_loss" value from current "profit_loss" value and add result to "change_profit_loss" list
-        change_profit_loss.append(int(row[1]) - int(profit_loss[-1]))
-        
 
         #for each row add profit_loss value to total_profit_loss to determine sum of column
         total_profit_loss = total_profit_loss + int(row[1])
 
-    print (str(change_profit_loss))
+    #print "Total Months" and "Total Profit/loss" to terminal
+    print (
+            "Financial Analysis"
+            + "\n----------------------------" 
+            + "\nTotal Months: " + str(len(date))
+            + "\nTotal: $" + str(total_profit_loss)
+            )   
+    
+    # iterate through "profit/loss list" to calculate average change in profit/loss
+    for value in range (1, len(profit_loss)):
+        #store result in "change_profit_loss variable"
+        change_profit_loss.append(int(profit_loss[value]) - int(profit_loss[value-1]))
 
-    def average(change_profit_loss):
-        averagechange_profit_loss = sum(change_profit_loss)/len(change_profit_loss)
+    # def average(change_profit_loss):
+        # averagechange_profit_loss = sum(change_profit_loss)/len(change_profit_loss)
 
-        return averagechange_profit_loss
+#         return averagechange_profit_loss
 
-#print analysis to terminal
-print (
-        "Financial Analysis"
-        + "\n----------------------------" 
-        + "\nTotal Months: " + str(len(date))
-        + "\nTotal: $" + str(total_profit_loss)
-        + "\nAverage  Change: $" + str(average(change_profit_loss))
-        )
+# print
+
