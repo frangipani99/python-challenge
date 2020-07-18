01606ae39dbf23c7dbfa42caa80d6e1e0c77d274
Cara Williams <carawilliams@Castros-MacBook-Pro-2.local>
2020-07-17 21:26:37 -0400

    add date to Greatest Decrease and Greatest Increase output

--- PyBank/main.py
+++ PyBank/main.py
@@ -40,15 +40,36 @@ with open(budgetdata_csv) as budgetdata_file:
     
     # iterate through "profit/loss list" to calculate average change in profit/loss
     for value in range (1, len(profit_loss)):
+        
         #store result in "change_profit_loss variable"
         change_profit_loss.append(int(profit_loss[value]) - int(profit_loss[value-1]))
 
+    #calulate average "Change in Profit Loss"
     def average(change_profit_loss):
         averagechange_profit_loss = sum(change_profit_loss)/len(change_profit_loss)
 
         return round((averagechange_profit_loss),2)
 
+    #remove first value from "Date" to align with "Change Profit/Loss list" list
+    date.pop(0)
+
+    #create a dictionary with "Date" and Average Change in Profit/loss" list   
+    change_profitloss_dict = dict(zip(date,change_profit_loss))
+
     #print "Average Change, Greatest Change, and Greatest Decrease in Profit/Loss, " 
     print ("Average  Change: $" + str(average(change_profit_loss)))
-    print ("Greatest Increase in Profits: " + str(max(change_profit_loss)))
-    print ("Greatest Decrease in Profits: " + str(min(change_profit_loss)))
+    print (
+            "Greatest Increase in Profits: " 
+            #print key in dictionary that matches "Greatest Increase"
+            + max(change_profitloss_dict, key=change_profitloss_dict.get)
+            + " "
+            + str(max(change_profit_loss))
+            )
+
+    print (
+            "Greatest Decrease in Profits: " 
+            #print key in dictionary that matches "Greatest Decrease"
+            + min(change_profitloss_dict, key=change_profitloss_dict.get)
+            + " "
+            + str(min(change_profit_loss))
+            )
